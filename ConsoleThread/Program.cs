5271bd41f717f713b9160249ed005891a9e02253
ngerasimov7 <nikola20@mail.ru>
2020-10-09 14:58:19 +0300

    Работа с потоками

---
+++ ConsoleThread/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Numerics;
+
+namespace ConsoleThread
+{
+    class Program
+    {
+        static BigInteger N;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для которого необходимо произвести расчет факториала и суммы целых чисел");
+            N = int.Parse(Console.ReadLine());
+
+            Thread Fthread = new Thread(new ThreadStart(fact))
+            {
+                Name = "Поток расчета факторила";
+            }
+            Fthread.Name=""
+            //Factorial.Text = fact(int.Parse(N.Text)).ToString();
+            //Summa.Text = summ(int.Parse(N.Text)).ToString();
+
+
+        }
+
+        static void fact()
+        {
+            Console.WriteLine($"Факториал числа {N} = {ffact(N)}");
+        }
+
+        static  BigInteger ffact(BigInteger num)
+        {
+            return (num == 0) ? 1 : num * ffact(num - 1);
+        }
+
+        static int summ(int num)
+        {
+            return (num == 0) ? 0 : num + summ(num - 1);
+        }
+    }
+}
--- Thread/MainWindow.xaml.cs
+++ Thread/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,7 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.nu
+using System.Numerics;
+
 
 namespace Thread
 {
@@ -28,9 +30,22 @@ namespace Thread
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Thread Fthread = new Thread();
+            
+            (new ThreadStart(fact(int.Parse(N.Text))));
+            //Factorial.Text = fact(int.Parse(N.Text)).ToString();
+            Summa.Text= summ(int.Parse(N.Text)).ToString();
+        }
+
+        public BigInteger fact(BigInteger num)
+        {
+            return (num == 0) ? 1 : num * fact(num - 1);
 
         }
 
-        private 
+        static int summ(int num)
+        {
+            return (num == 0) ? 0 : num + summ(num - 1);
+        }
     }
 }
